a02a39202252828e0910d60f990ae9f0f8d9aff9
Maxime <forestmaxime25@gmail.com>
2015-09-18 19:57:56 +0200

    ajout file system

---
+++ TpFileSystem/Directory.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPFileSystem
+{
+      class Directory : Fichier
+    {
+        public List<Fichier> Fichiers = new List<Fichier>();
+
+        public Directory(string Nom, Directory Parent) : base(Nom, Parent) {
+            
+        }
+
+
+
+        public override bool mkdir(string nom)
+        {
+            Directory newDirectory = new Directory(nom, this);
+            Fichiers.Add(newDirectory);
+            Console.WriteLine("Directory créé");
+            
+            return true;
+        }
+
+        public Fichier cd(string name)
+        {
+            
+            foreach (Fichier file in Fichiers)
+            {
+                if (file.Nom == name)
+                {
+                    return file;
+                }
+                
+            }
+            return null;
+        }
+
+        public virtual bool createNewFile(string name)
+        {
+            Fichier newFichier = new Fichier(name, this);
+            Fichiers.Add(newFichier);
+            Console.WriteLine("Fichier créé");
+            return true;
+        }
+
+        public List<Fichier> ls()
+        {
+            return this.Fichiers;
+        }
+        public List<Fichier> search(string name)
+        {
+            List<Fichier> retour = null;
+
+
+            retour = new List<Fichier>();
+
+            foreach (Fichier searchDir in Fichiers)
+            {
+
+                if (searchDir.Nom == name)
+                {
+                    retour.Add(searchDir);
+                }
+
+                
+                List<Fichier> retour2 = new List<Fichier>();
+                Directory searchDir2 = (Directory)searchDir;
+                retour2 = searchDir2.search(name);
+                    foreach (Fichier courant in retour2)
+                    {
+                        retour.Add(courant);
+                    }
+            }
+            return retour;
+        }
+
+        public bool rename(string nomFichier, string nouveauNom)
+        {
+            foreach (Fichier file in Fichiers)
+            {
+                if (file.Nom == nomFichier)
+                {
+                    file.Nom = nouveauNom;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+
+        public bool delete(string name)
+        {
+                foreach (Fichier file in Fichiers)
+                {
+
+                    if (file.Nom == name)
+                    {
+                        Fichiers.Remove(file);
+                        return true;
+                    }
+
+                }
+                return false;
+        }
+
+
+            
+      }
+    }
+   
---
+++ TpFileSystem/Fichier.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPFileSystem
+{
+     class Fichier
+    {
+         public Directory Parent=null;
+         public string Nom {get;set;}
+         public int permission = 4;
+        
+       
+
+        public Fichier(string Nom, Directory Parent) {
+            
+            this.Nom = Nom;
+            this.Parent = Parent;
+
+        }
+
+
+
+        public bool canWrite()
+        {
+            return (permission & 2) > 0;
+        }
+        public bool canExecute()
+        {
+            return (permission & 1) > 0;
+        }
+        public bool canRead()
+        {
+            return (permission & 4) > 0;
+        }
+
+        public void chmod(int permission)
+        {
+
+            this.permission = permission;
+        }
+
+
+
+        public bool isFile() 
+        {
+            if (this.GetType() == typeof(Fichier))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool isDirectory(){
+            if(this.GetType()==typeof(Directory))
+            {
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        public virtual bool mkdir(string name) {
+            return false;
+        }
+
+
+        public Fichier getParent()
+        {
+            if (this.Nom != "/")
+                return this.Parent;
+            else
+            {
+                Console.WriteLine("Je suis la racine !");
+                return this;
+            }
+
+        }
+
+        public string getPath()
+        {
+            Fichier pathFile = this;
+            string path = "";
+
+            while (pathFile.Nom != "/")
+            {
+
+                path = pathFile.Nom + "/" + path;
+                pathFile = pathFile.Parent;
+
+            }
+
+            return path;
+
+        }
+
+        public string getPermission()
+        {
+            string permission = "";
+            if (this.canExecute())
+            {
+                permission = permission + "x";
+            }
+            else if (this.canRead())
+            {
+                permission = permission + "r";
+            }
+            else if (this.canWrite())
+            {
+                permission = permission + "w";
+            }
+            else
+            {
+                permission = permission + "-";
+            }
+            return permission;
+        }
+
+
+
+
+    }
+}
